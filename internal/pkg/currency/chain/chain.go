bebe13b0b64a65154c9017650588783010d40525
Anton Uvarenko <anton.uvarenko@LT-M-6828.local>
2024-06-29 08:34:27 +0300

    feat: error logging

--- internal/pkg/currency/chain/chain.go
+++ internal/pkg/currency/chain/chain.go
@@ -1,6 +1,8 @@
 package chain
 
 import (
+	"fmt"
+
 	"github.com/anton-uvarenko/backend_school/internal/pkg"
 	"github.com/anton-uvarenko/backend_school/internal/pkg/currency/provider"
 )
@@ -28,6 +30,7 @@ func (c *BaseChain) SetNext(next ProvidersChain) {
 func (c *BaseChain) GetUAHToUSD() (float32, error) {
 	rate, err := c.provider.GetUAHToUSD()
 	if err == nil {
+		fmt.Printf("error receving rate: %v\n", err)
 		return rate, nil
 	}
 
